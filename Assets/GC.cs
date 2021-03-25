c236b1f7b91162eebaec00267ff7a97365c495f6
metehanguclu <mete53guclu@gmail.com>
2021-03-25 16:49:56 +0300

    Attack

---
+++ Assets/GC.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GC : MonoBehaviour
+{
+    public GameOverScreen GameOverScreen;
+
+    public void GameOver()
+    {
+        GameOverScreen.Setup();
+        Debug.Log("dfhs");
+    }
+}
---
+++ Assets/RespawnScreen.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnScreen : MonoBehaviour
+{
+    public void Start()
+    {
+       // FindObjectOfType<GameManager>().EndGame();
+    }
+    
+}
