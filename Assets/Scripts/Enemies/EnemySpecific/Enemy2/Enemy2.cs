5f883c8be782b992503185885ec4bb87cca99a8f
metehanguclu <mete53guclu@gmail.com>
2021-04-29 23:50:34 +0300

    New Character

---
+++ Assets/Scripts/Enemies/EnemySpecific/Enemy2/Enemy2.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy2 : Entity
+{   
+    public E2_MoveState moveState { get; private set; }
+    public E2_IdleState idleState { get; private set; }
+    public E2_PlayerDetectedState playerDetectedState { get; private set; }
+    public E2_MeleeAttackState meleeAttackState { get; private set; }
+    public E2_LookForPlayerState lookForPlayerState { get; private set; }
+    public E2_StunState stunState { get; private set; }
+    public E2_DeadState deadState { get; private set; }
+    public E2_DodgeState dodgeState { get; private set; }
+    public E2_RangedAttackState rangedAttackState { get; private set; }
+
+    [SerializeField]
+    private D_MoveState moveStateData;
+    [SerializeField]
+    private D_IdleState idleStateData;
+    [SerializeField]
+    private D_PlayerDetected playerDetectedStateData;
+    [SerializeField]
+    private D_MeleeAttack meleeAttackStateData;
+    [SerializeField]
+    private D_LookForPlayer lookForPlayerStateData;
+    [SerializeField]
+    private D_StunState stunStateData;
+    [SerializeField]
+    private D_DeadState deadStateData;
+    [SerializeField]
+    public D_DodgeState dodgeStateData; 
+    [SerializeField]
+    private D_RangedAttackState rangedAttackStateData;
+
+    [SerializeField]
+    private Transform meleeAttackPosition;
+    [SerializeField]
+    private Transform rangedAttackPosition;
+
+    [SerializeField]
+    private Animator enemyAnim;
+
+    private GameObject aliveEnemy;
+
+    public float CamShakeAmt = 0.1f;
+    CameraShake camShake;
+
+    public override void Start()
+    {
+        base.Start();
+
+        moveState = new E2_MoveState(this, stateMachine, "move", moveStateData, this);
+        idleState = new E2_IdleState(this, stateMachine, "idle", idleStateData, this);
+        playerDetectedState = new E2_PlayerDetectedState(this, stateMachine, "playerDetected", playerDetectedStateData, this);
+        meleeAttackState = new E2_MeleeAttackState(this, stateMachine, "meleeAttack", meleeAttackPosition, meleeAttackStateData, this);
+        lookForPlayerState = new E2_LookForPlayerState(this, stateMachine, "lookForPlayer", lookForPlayerStateData, this);
+        stunState = new E2_StunState(this, stateMachine, "stun", stunStateData, this);
+        deadState = new E2_DeadState(this, stateMachine, "dead", deadStateData, this);
+        dodgeState = new E2_DodgeState(this, stateMachine, "dodge", dodgeStateData, this);
+        rangedAttackState = new E2_RangedAttackState(this, stateMachine, "rangedAttack", rangedAttackPosition, rangedAttackStateData, this);
+
+        aliveEnemy = transform.Find("Alive").gameObject;
+        enemyAnim = aliveEnemy.GetComponent<Animator>();
+        aliveGO.SetActive(true);
+
+        stateMachine.Initialize(moveState);
+
+        camShake = GameObject.FindGameObjectWithTag("GM").GetComponent<GameManager>().GetComponent<CameraShake>();
+    }
+
+    public override void Damage(AttackDetails attackDetails)
+    {
+        base.Damage(attackDetails);
+        camShake.Shake(CamShakeAmt, 0.1f);
+
+        if (isDead)
+        {
+            stateMachine.ChangeState(deadState);
+            enemyAnim.SetBool("dead", true);
+
+            Object.Destroy(gameObject, 10.0f);
+        }
+        else if(isStunned && stateMachine.currentState != stunState)
+        {
+            stateMachine.ChangeState(stunState);
+        }
+        else if (CheckPlayerInMinAgroRange())
+        {
+            stateMachine.ChangeState(rangedAttackState);
+        }
+        else if (!CheckPlayerInMinAgroRange())
+        {
+            lookForPlayerState.SetTurnImmediately(true);
+            stateMachine.ChangeState(lookForPlayerState);
+        }
+    }
+
+    public override void OnDrawGizmos()
+    {
+        base.OnDrawGizmos();
+
+        Gizmos.DrawWireSphere(meleeAttackPosition.position, meleeAttackStateData.attackRadius);
+    }
+}
